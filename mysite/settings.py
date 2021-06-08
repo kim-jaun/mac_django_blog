e4bbbcd0394f0d1b023eebd84f513e483aa42adf
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-06-08 17:40:12 +0900

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'me)qx5bpc!9c(#zf+i=m1nu^@l45gt7on319in+k1@2mu89a@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
